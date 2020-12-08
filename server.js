5ad12f8278f4ec95198f4a6417d64bfc633d40d1
JacGenato <jac_genato@yahoo.com>
2020-12-08 19:33:20 +0800

    Revert "Prepare for deploy"
    This reverts commit 7a528a040bfe23ad16f37b2d3eb07bc73d246f37.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
 
 const app = express();
 
@@ -10,21 +9,14 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the BJCLC' }));
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the BJCLC' })
+);
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/sermons', require('./routes/sermons'));
-
-// serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.statis('client/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
 
 const PORT = process.env.PORT || 5000;
 
