24cddb5428aa57fb57059d03d53c4f8232041b1c
JacGenato <jac_genato@yahoo.com>
2020-12-08 19:52:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/sermons', require('./routes/sermons'));
 // serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.statis('client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
