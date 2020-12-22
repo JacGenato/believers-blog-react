e7b19b68efcb1cd68baa83bd948c80878913bb6d
JacGenato <jac_genato@yahoo.com>
2020-12-22 13:02:03 +0800

    deploy maintenance

--- server.js
+++ server.js
@@ -19,8 +19,11 @@ app.use('/api/lessons', require('./routes/sermons'));
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  app.get(
+    '*',
+    (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+    res.sendFile(path.resolve(__dirname, 'app', 'build', 'index.html'))
   );
 }
 
