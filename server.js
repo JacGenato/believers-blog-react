94f6ad532ecdf1d8282d99a5b7eb4d1ae3de5ce1
JacGenato <jac_genato@yahoo.com>
2020-12-22 14:06:44 +0800

    CORS

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ app.use('/api/lessons', require('./routes/sermons'));
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
-  app.get('*', (req, res) =>
+  app.get(
+    '*',
+    (req, res) => res.header('Access-Control-Allow-Origin', '*'),
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    ),
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
