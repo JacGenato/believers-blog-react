8132ff78fbccaa2dbd7d9374d14fac7a414db3c3
JacGenato <jac_genato@yahoo.com>
2020-12-22 14:10:04 +0800

    Revert "CORS"
    This reverts commit 94f6ad532ecdf1d8282d99a5b7eb4d1ae3de5ce1.

--- server.js
+++ server.js
@@ -19,13 +19,7 @@ app.use('/api/lessons', require('./routes/sermons'));
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
-  app.get(
-    '*',
-    (req, res) => res.header('Access-Control-Allow-Origin', '*'),
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    ),
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
