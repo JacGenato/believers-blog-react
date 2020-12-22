6767fca13d733f179b823f77c9c27fcb64dd9855
JacGenato <jac_genato@yahoo.com>
2020-12-22 12:31:09 +0800

    heroku deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-//app.get('/', (req, res) => res.json({ msg: 'Welcome to the BJCLC' }));
+const publicPath = path.join(__dirname, '..', 'public');
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -18,13 +18,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/lessons', require('./routes/sermons'));
 
 // serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//   // set static folder
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   );
-// }
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
